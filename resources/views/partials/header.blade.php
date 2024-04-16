f5dd2547b4785d34c4f5a2c3176a1830e0a7fbdb
lackbyte12 <codmosjethroczar@gmail.com>
2024-04-16 18:26:29 +0800

    init

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+<header class="bg-red text-white text-center py-4">
+    <h1 class="text-2xl font-bold mx-auto" style="max-width: 90%;">My Book Collection</h1>
+    @vite('resources/css/app.css')
+</header>
+</head>
+<body class="bg-brown">
